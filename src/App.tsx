d0628b6931f3cae6943b3b06d397f7ac0fabcdde
ptimer <kriminalmykriminal@gmail.com>
2024-10-18 17:06:33 +0200

    feat: ribbon

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Ribbon from './components/Ribbon';
 import { NoteProvider } from './context/NoteContext';
 import NotesPage from './pages/NotesPage';
 
@@ -5,7 +6,15 @@ function App() {
   return (
     <div id='app'>
       <NoteProvider>
-        <NotesPage />
+        <>
+          <NotesPage />
+          <Ribbon
+            position="right-bottom"
+            title="View Source on GitHub"
+            url={"https://github.com/ptimer/sticky_notes"}
+            baseColour="dark-gray"
+          />
+        </>
       </NoteProvider>
     </div>
   )
